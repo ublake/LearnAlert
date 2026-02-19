06a13fb1b36d39cf0de14b7801e000ec446df064
Blake Miller <ttw55970@gmail.com>
2026-02-19 11:21:28 -0500

    Initial Commit

---
+++ LearnAlert2026/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LearnAlert2026
+//
+//  Created by Blake Miller on 2/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnAlert2026/LearnAlert2026App.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAlert2026App.swift
+//  LearnAlert2026
+//
+//  Created by Blake Miller on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct LearnAlert2026App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
